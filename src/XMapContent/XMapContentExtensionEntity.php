ea20d30d06b19f6fdfaf4bfda976614bc60967d5
Sinri Edogawa <e.joshua.s.e@gmail.com>
2019-10-23 11:23:17 +0800

    others of Topic fix

--- src/XMapContent/XMapContentExtensionEntity.php
+++ src/XMapContent/XMapContentExtensionEntity.php
@@ -14,6 +14,24 @@ class XMapContentExtensionEntity extends XMapNodeEntity
      * Note: if multiple topic extensions have the same provider, only the first one will be available.
      */
     protected $attrProvider;
+
+    /**
+     * @return string
+     */
+    public function getAttrProvider()
+    {
+        return $this->attrProvider;
+    }
+
+    /**
+     * @param string $attrProvider
+     * @return XMapContentExtensionEntity
+     */
+    public function setAttrProvider($attrProvider)
+    {
+        $this->attrProvider = $attrProvider;
+        return $this;
+    }
     /**
      * @var XMapNodeEntity [0,1] the content of this extension
      * Format Free
@@ -21,8 +39,45 @@ class XMapContentExtensionEntity extends XMapNodeEntity
      * This element is not restricted and is available to store any information provided by the extension provider.
      */
     protected $content;
+
     /**
-     * @var ? [0,1] the container of resource references used by this extension
+     * @return XMapNodeEntity
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param XMapNodeEntity $content
+     * @return XMapContentExtensionEntity
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+    /**
+     * @return XMapContentResourceRefsEntity
+     */
+    public function getResourceRefs()
+    {
+        return $this->resourceRefs;
+    }
+
+    /**
+     * @param XMapContentResourceRefsEntity $resourceRefs
+     * @return XMapContentExtensionEntity
+     */
+    public function setResourceRefs($resourceRefs)
+    {
+        $this->resourceRefs = $resourceRefs;
+        return $this;
+    }
+
+    /**
+     * @var XMapContentResourceRefsEntity [0,1] the container of resource references used by this extension
      */
     protected $resourceRefs;
 
@@ -37,6 +92,10 @@ class XMapContentExtensionEntity extends XMapNodeEntity
      */
     protected function writeThisNode($xmlWriter)
     {
-        // TODO: Implement writeThisNode() method.
+        $xmlWriter->startElement($this->nodeTag());
+        $xmlWriter->writeAttribute("provider", $this->attrProvider);
+        self::writeThatNode($xmlWriter, $this->content);
+        self::writeThatNode($xmlWriter, $this->resourceRefs);
+        $xmlWriter->endElement();
     }
 }
